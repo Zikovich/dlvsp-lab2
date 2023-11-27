9020d663d63f89a4d9088c83338f3a31d60a9155
Zikovich <islam.zakariaa@gmail.com>
2023-11-27 19:21:14 +0100

    fix prediction error in opencv

--- demo/predictor.py
+++ demo/predictor.py
@@ -607,8 +607,9 @@ class VIDDemo(object):
         for box, score, label in zip(boxes, scores, labels):
             x, y = box[:2]
             s = template.format(label, score)
+            print ("x =",int(x.item()))
             cv2.putText(
-                image, s, (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2
+                image, s, (int(x.item()), int(y.item())), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2
             )
 
         return image
